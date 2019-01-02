effe5c84c9e5391cdc27ade3e7f2c6899046752b
renyikai1 <754376749@qq.com>
2019-01-02 23:39:39 +0800

    第一次提交

---
+++ src/test/Hello.java
@@ -0,0 +1,16 @@
+package test;
+/** 
+
+* @author renyikai
+
+* @version 创建时间：2019年1月2日 下午10:54:48 
+
+* 
+
+*/
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println("你好");
+	}
+
+}
